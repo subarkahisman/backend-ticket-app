fa83edd8b4b10897f8b13f5a6d5c9caa79d4bbe7
subarkahisman <ismansubarkah.2210@gmail.com>
2025-05-23 19:37:55 +0700

    Add Event model and add some code on User Model

--- src/test/java/com/rashcode/tickets/domain/User.java
+++ src/test/java/com/rashcode/tickets/domain/User.java
@@ -1,14 +1,13 @@
 package com.rashcode.tickets.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -29,6 +28,25 @@ public class User {
 
     @Column(name = "email", nullable = false)
     private String email;
+
+    @OneToMany(mappedBy = "organizer", cascade = CascadeType.ALL)
+    private List<Event> organizedEvents = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name="user_attending_events",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private List<Event> attendingEvents = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_staffing_events",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private List<Event> staffingEvents = new ArrayList<>()
 
     @CreatedDate
     @Column(name = "created_at", updatable = false, nullable = false)
